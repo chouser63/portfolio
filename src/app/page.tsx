6040ec155bbecd2c71d96c1952e5561f65e24dcf
chouser63 <houser.ch@northeastern.edu>
2025-10-09 22:48:57 -0700

    implemented a useless component that only serves to enrage others, this is anti-human computing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { ProgressClicker } from "@/components/progress-clicker";
+import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
 
 export default function Home() {
+
+  
+
   return (
-    <div className="">
+    <div className="w-full h-full flex flex-col items-center justify-center pt-6 space-y-6">
+
+      <ProgressClicker/>
         
     </div>
   );
