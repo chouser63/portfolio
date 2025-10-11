7eae0706a51564d26ca5242917ea884bbb91d96c
chouser63 <houser.ch@northeastern.edu>
2025-10-11 16:45:56 -0700

    added socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
 import { ProgressClicker } from "@/components/progress-clicker";
 import ProfilePicture from "@/components/ui/profile-picture";
 import { Progress } from "@/components/ui/progress";
+import { Github } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { SocialIcon } from 'react-social-icons'
+
 
 
 export default function Home() {
 
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center py-6 space-y-12">
+    <div className="w-full h-full flex flex-col items-center justify-center py-12 space-y-12">
       <div className="w-2/3 flex flex-col sm:flex-row items-center justify-center space-y-12 sm:space-y-0 sm:space-x-12">
         <ProfilePicture src="/images/profile-photo.jpg" size={300} />
         <h1 className="text-4xl font-bold tracking-tight text-slate-900 sm:text-5xl dark:text-white">
           Hi, I'm Chase.
         </h1>
       </div>
-      <div className="w-2/3 sm:w-1/2 space-y-6 text-lg text-slate-700 dark:text-white text-center">
+      <div className="w-3/4 sm:w-1/2 space-y-6 text-xl text-slate-700 dark:text-white">
+        <div className="flex flex-row space-x-6">
+          <SocialIcon url="https://www.github.com/chouser63" target="_blank" rel="noopener noreferrer"/>
+          <SocialIcon url="https://www.linkedin.com/in/chase-houser/" target="_blank" rel="noopener noreferrer"/>
+        </div>
         <p>
           I'm a third-year Computer Science and Business Administration student at Northeastern University with a deep passion for learning new things and a belief in having a wide breadth of experiences.
         </p>
@@ -30,18 +38,18 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="w-2/3 sm:w-1/2 space-y-6 text-lg text-slate-700 dark:text-white">
-        <h1 className="text-2xl font-bold tracking-tight text-slate-900 sm:text-3xl dark:text-white">
+      <div className="w-3/4 sm:w-1/2 space-y-4 text-xl text-slate-700 dark:text-white">
+        <h1 className="text-3xl font-bold tracking-tight text-slate-900 dark:text-white">
           Contact
         </h1>
         <p>
           houser.ch@northeastern.edu
-          <br/>
+          <br />
           +1(847)-284-5315
         </p>
       </div>
 
-      <div className="w-2/3 sm:w-1/2">
+      <div className="w-3/4 sm:w-1/2">
         <ProgressClicker />
       </div>
 
