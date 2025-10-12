90144344675775257c05b097e233b5a90c195f7d
chouser63 <houser.ch@northeastern.edu>
2025-10-12 11:19:00 -0700

    formatting

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -4,17 +4,17 @@ import { Photo } from "@/components/ui/photo-gallery";
 export default function Media() {
 
     let photos: Photo[] = [
-    { src: "/images/gallery1.JPG", description: "Libertyville, IL" },
-    { src: "/images/gallery2.JPG", description: "Libertyville, IL" },
-    { src: "/images/gallery3.JPG", description: "Taken in Badlands National Park on a road trip my brother and I took from Illinois to Seattle." },
-    { src: "/images/gallery4.JPG", description: "My brother Cole standing at the edge of a cliff in Badlands." },
-    { src: "/images/gallery5.JPG", description: "Cannon Mountain, Franciona, NH" },
-    { src: "/images/gallery6.JPG", description: "Us?" },
-    { src: "/images/gallery7.JPG", description: "Mission Hill, Boston" },
-    { src: "/images/gallery8.JPG", description: "Mission Hill, Boston" },
-    { src: "/images/gallery9.JPG", description: "Mission Hill, Boston + Blue the dog" },
-    { src: "/images/gallery10.JPG", description: "Cannon Mountain, Franciona, NH" }
-];
+        { src: "/images/gallery1.JPG", description: "Libertyville, IL" },
+        { src: "/images/gallery2.JPG", description: "Libertyville, IL" },
+        { src: "/images/gallery3.JPG", description: "Badlands National Park, South Dakota" },
+        { src: "/images/gallery4.JPG", description: "My brother in Badlands National Park" },
+        { src: "/images/gallery5.JPG", description: "Cannon Mountain, Franciona, NH" },
+        { src: "/images/gallery6.JPG", description: "Us?" },
+        { src: "/images/gallery7.JPG", description: "Mission Hill, Boston" },
+        { src: "/images/gallery8.JPG", description: "Mission Hill, Boston" },
+        { src: "/images/gallery9.JPG", description: "Mission Hill, Boston + Blue the dog" },
+        { src: "/images/gallery10.JPG", description: "Cannon Mountain, Franciona, NH" }
+    ];
 
     photos.sort(() => Math.random() - 0.5);
 
@@ -23,7 +23,5 @@ export default function Media() {
             <PhotoGallery photos={photos} />
         </div>
     )
-
-
 
 }
