a334cf16cd5527faba2609967824807761864cc0
chouser63 <houser.ch@northeastern.edu>
2025-11-06 13:04:16 -0800

    added movie review page and block component and added fetching logic from supabase

--- src/app/movie-reviews/page.tsx
+++ src/app/movie-reviews/page.tsx
@@ -1,8 +1,27 @@
-export default function MovieReviews() {
+import MovieReviewCard from "@/components/movie-reviews/movie-review-block";
+import { supabase } from "@/lib/supabaseClient";
+
+export default async function MovieReviews() {
+
+// Fetch data
+    const { data, error } = await supabase.from("MovieReviews").select().order("rank", { ascending: true });
+
+    // Optional: handle error
+    if (error) {
+        console.error("Error fetching movie reviews:", error.message);
+        return <div>Failed to load movie reviews.</div>;
+    }
+
+    // data can be null if no rows, so default to empty array
+    const reviews = data ?? [];
 
     return (
-        <div className="w-full h-full flex flex-col items-center justify-center pt-6">
-            Gooners unite!
+        <div className="w-full h-full flex flex-col items-center justify-center pt-6 px-12">
+            <div className="flex flex-col gap-4">
+                {reviews.map((r) => (
+                    <MovieReviewCard key={r.id} review={r} />
+                ))}
+            </div>
         </div>
     )
 }
